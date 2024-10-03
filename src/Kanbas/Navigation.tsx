13340fbc403a468fae7525e81cf9b8dd4829f292
harryzhang <harryzhang@bjcjitec.com>
2024-10-03 14:30:17 -0700

    Add dynamic navigation side bar

--- src/Kanbas/Navigation.tsx
+++ src/Kanbas/Navigation.tsx
@@ -1,52 +1,103 @@
+import { NavLink } from "react-router-dom";
 import { AiOutlineDashboard } from "react-icons/ai";
 import { IoCalendarOutline } from "react-icons/io5";
 import { LiaBookSolid, LiaCogSolid } from "react-icons/lia";
 import { FaInbox, FaRegCircleUser } from "react-icons/fa6";
 import { FaHome } from "react-icons/fa";
-import { Link } from "react-router-dom";
+
 export default function KanbasNavigation() {
+
+
   return (
-    <div id="wd-kanbas-navigation" style={{ width: 120 }}
-         className="list-group rounded-0 position-fixed
-         bottom-0 top-0 d-none d-md-block bg-black z-2">
-      <a id="wd-neu-link" target="_blank"
-         href="https://www.northeastern.edu/"
-         className="list-group-item bg-black border-0 text-center">
-        <img src="/images/NEU.png" width="75px"/></a>
-      <Link to="/Kanbas/Account" id="wd-account-link"
-        className="list-group-item text-center border-0
-                   bg-black text-white">
-        <FaRegCircleUser className="fs-1 text text-white" /><br />
-        Account</Link>
-      <Link to="/Kanbas/Dashboard" id="wd-dashboard-link"
-        className="list-group-item text-center border-0
-                   bg-white text-danger">
-        <AiOutlineDashboard className="fs-1 text-danger" /><br />
-        Dashboard</Link>
-      <Link to="/Kanbas/Dashboard" id="wd-course-link"
-        className="list-group-item text-center border-0
-                   bg-black text-danger">
-        <LiaBookSolid className="fs-1 text-danger" /><br />
-            Courses</Link>
-      <Link to="/Kanbas/Calendar" id="wd-calendar-link"
-        className="list-group-item text-center border-0
-                   bg-black text-danger">
-        <IoCalendarOutline className="fs-1 text-danger" /><br />
-            Calendar</Link>
-      <Link to="/Kanbas/Inbox" id="wd-inbox-link"
-        className="list-group-item text-center border-0
-                   bg-black text-danger">
-        <FaInbox className="fs-1 text-danger" /><br />
-            Inbox</Link>
-      <Link to="/Labs" id="wd-labs-link"
-        className="list-group-item text-center border-0
-                   bg-black text-danger">
-        <LiaCogSolid className="fs-1 text-danger" /><br />
-        Labs</Link>
-      <Link to="/"
-        className="list-group-item text-center border-0
-                   bg-black text-danger">
-        <FaHome className="fs-1 text-danger" /><br />
-            Landing Page</Link>
+    <div
+      id="wd-kanbas-navigation"
+      style={{ width: 120 }}
+      className="list-group rounded-0 position-fixed bottom-0 top-0 d-none d-md-block bg-black z-2"
+    >
+      <a
+        id="wd-neu-link"
+        target="_blank"
+        href="https://www.northeastern.edu/"
+        className="list-group-item bg-black border-0 text-center"
+      >
+        <img src="/images/NEU.png" width="75px" />
+      </a>
+
+      <NavLink
+        to="/Kanbas/Account"
+        id="wd-account-link"
+        className={({ isActive }) =>
+          isActive ? 'list-group-item text-center border-0 bg-white text-danger' : 'list-group-item text-center border-0 bg-black text-white'
+        }>
+        <FaRegCircleUser className="fs-1 text-danger" />
+        <br />
+        Account
+      </NavLink>
+
+      <NavLink
+        to="/Kanbas/Dashboard"
+        id="wd-dashboard-link"
+        className={({ isActive }) =>
+          isActive ? 'list-group-item text-center border-0 bg-white text-danger' : 'list-group-item text-center border-0 bg-black text-white'
+        }>
+        <AiOutlineDashboard className="fs-1 text-danger" />
+        <br />
+        Dashboard
+      </NavLink>
+
+      <NavLink
+        to="/Kanbas/Courses"
+        id="wd-course-link"
+        className={({ isActive }) =>
+          isActive ? 'list-group-item text-center border-0 bg-white text-danger' : 'list-group-item text-center border-0 bg-black text-white'
+        }>
+        <LiaBookSolid className="fs-1 text-danger" />
+        <br />
+        Courses
+      </NavLink>
+
+      <NavLink
+        to="/Kanbas/Calendar"
+        id="wd-calendar-link"
+        className={({ isActive }) =>
+          isActive ? 'list-group-item text-center border-0 bg-white text-danger' : 'list-group-item text-center border-0 bg-black text-white'
+        }>
+        <IoCalendarOutline className="fs-1 text-danger" />
+        <br />
+        Calendar
+      </NavLink>
+
+      <NavLink
+        to="/Kanbas/Inbox"
+        id="wd-inbox-link"
+        className={({ isActive }) =>
+          isActive ? 'list-group-item text-center border-0 bg-white text-danger' : 'list-group-item text-center border-0 bg-black text-white'
+        }>
+        <FaInbox className="fs-1 text-danger" />
+        <br />
+        Inbox
+      </NavLink>
+
+      <NavLink
+        to="/Labs"
+        id="wd-labs-link"
+        className={({ isActive }) =>
+          isActive ? 'list-group-item text-center border-0 bg-white text-danger' : 'list-group-item text-center border-0 bg-black text-white'
+        }>
+        <LiaCogSolid className="fs-1 text-danger" />
+        <br />
+        Labs
+      </NavLink>
+
+      <NavLink
+        to="/"
+        className={({ isActive }) =>
+          isActive ? 'list-group-item text-center border-0 bg-white text-danger' : 'list-group-item text-center border-0 bg-black text-white'
+        }>
+        <FaHome className="fs-1 text-danger" />
+        <br />
+        Landing Page
+      </NavLink>
     </div>
-  );}
+  );
+}
