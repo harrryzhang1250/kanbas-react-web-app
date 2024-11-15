1416c2fd2564449019098d5ac193bc7bc97a89a7
harryzhang <harryzhang@bjcjitec.com>
2024-11-14 23:07:22 -0800

    Add enrollment section

--- src/Kanbas/Account/client.ts
+++ src/Kanbas/Account/client.ts
@@ -1,13 +1,5 @@
 import axios from "axios";
 const axiosWithCredentials = axios.create({ withCredentials: true });
-export const findMyCourses = async () => {
-  const { data } = await axiosWithCredentials.get(`${USERS_API}/current/courses`);
-  return data;
-};
-export const createCourse = async (course: any) => {
-  const { data } = await axiosWithCredentials.post(`${USERS_API}/current/courses`, course);
-  return data;
-};
 
 export const REMOTE_SERVER = process.env.REACT_APP_REMOTE_SERVER;
 export const USERS_API = `${REMOTE_SERVER}/api/users`;
@@ -33,6 +25,20 @@ export const signout = async () => {
   const response = await axiosWithCredentials.post(`${USERS_API}/signout`);
   return response.data;
 };
+
+export const findMyCourses = async () => {
+  const { data } = await axiosWithCredentials.get(`${USERS_API}/current/courses`);
+  return data;
+};
+export const createCourse = async (course: any) => {
+  const { data } = await axiosWithCredentials.post(`${USERS_API}/current/courses`, course);
+  return data;
+};
+
+export const findAllMyEnrollments = async (userId: string) => {
+  const response = await axiosWithCredentials.get(`${USERS_API}/${userId}/enrollments`);
+  return response.data
+}
 
 
 
--- src/Kanbas/Dashboard/enrollmentsClient.ts
+++ src/Kanbas/Dashboard/enrollmentsClient.ts
@@ -1,3 +1,12 @@
 import axios from "axios";
 const REMOTE_SERVER = process.env.REACT_APP_REMOTE_SERVER;
-const COURSES_API = `${REMOTE_SERVER}/api/courses`;
+
+
+export const addEnrollment = async (userId: string,  courseId: string) => {
+  const response = await axios.post(`${REMOTE_SERVER}/${userId}/${courseId}/enroll`);
+  return response.data;
+}
+export const deleteEnrollment = async (userId: string,  courseId: string) => {
+  const response = await axios.delete(`${REMOTE_SERVER}/${userId}/${courseId}/unenroll`);
+  return response.data;
+}
